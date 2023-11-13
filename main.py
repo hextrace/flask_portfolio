54cdb47c663e03e770ae0053678c0a9b9e02d6d7
hextrace <iocontrol808@gmail.com>
2023-11-13 18:24:37 -0500

    Cleaned up some code.

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def inject_header():
       href="./static/assets/img/apple-icon.png"
     />
     <link rel="icon" type="image/png" href="./static/assets/img/favicon.png" />
-    <title>Portfolio• Kirk Wallace</title>
+    <title>Portfolio • Kirk Wallace</title>
     <!--     Fonts and icons     -->
     <link
       href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800"
@@ -155,134 +155,74 @@ def inject_footer():
             <div class="container">
               <div class="row">
                 <div class="col-md-3">
-                  <h1 class="title">BLK•</h1>
+                  <h1 class="title">K • W</h1>
                 </div>
                 <div class="col-md-3">
                   <ul class="nav">
                     <li class="nav-item">
-                      <a href="../index.html" class="nav-link"> Home </a>
+                      <a href="/index.html" class="nav-link"> Home </a>
                     </li>
                     <li class="nav-item">
-                      <a href="../examples/landing-page.html" class="nav-link">
-                        Landing
+                      <a href="/projects.html" class="nav-link">
+                        Projects
                       </a>
                     </li>
                     <li class="nav-item">
-                      <a href="../examples/register-page.html" class="nav-link">
-                        Register
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="../examples/profile-page.html" class="nav-link">
-                        Profile
+                      <a href="/profile-page.html" class="nav-link">
+                        About Me
                       </a>
                     </li>
                   </ul>
                 </div>
                 <div class="col-md-3">
-                  <ul class="nav">
-                    <li class="nav-item">
-                      <a
-                        href="https://creative-tim.com/contact-us"
-                        class="nav-link"
-                      >
-                        Contact Us
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="https://creative-tim.com/about-us" class="nav-link">
-                        About Us
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="https://creative-tim.com/blog" class="nav-link">
-                        Blog
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a
-                        href="https://opensource.org/licenses/MIT"
-                        class="nav-link"
-                      >
-                        License
-                      </a>
-                    </li>
-                  </ul>
+                  &nbsp;
                 </div>
                 <div class="col-md-3">
-                  <h3 class="title">Follow us:</h3>
-                  <div class="btn-wrapper profile">
-                    <a
-                      target="_blank"
-                      href="https://twitter.com/creativetim"
-                      class="btn btn-icon btn-neutral btn-round btn-simple"
-                      data-toggle="tooltip"
-                      data-original-title="Follow us"
-                    >
-                      <i class="fab fa-twitter"></i>
-                    </a>
-                    <a
-                      target="_blank"
-                      href="https://www.facebook.com/creativetim"
-                      class="btn btn-icon btn-neutral btn-round btn-simple"
-                      data-toggle="tooltip"
-                      data-original-title="Like us"
-                    >
-                      <i class="fab fa-facebook-square"></i>
-                    </a>
-                    <a
-                      target="_blank"
-                      href="https://dribbble.com/creativetim"
-                      class="btn btn-icon btn-neutral btn-round btn-simple"
-                      data-toggle="tooltip"
-                      data-original-title="Follow us"
-                    >
-                      <i class="fab fa-dribbble"></i>
-                    </a>
-                  </div>
+                  &nbsp;
                 </div>
               </div>
             </div>
-          </footer>
-        </div>
-        <!--   Core JS Files   -->
-        <script
-          src="./static/assets/js/core/jquery.min.js"
-          type="text/javascript"
-        ></script>
-        <script
-          src="./static/assets/js/core/popper.min.js"
-          type="text/javascript"
-        ></script>
-        <script
-          src="./static/assets/js/core/bootstrap.min.js"
-          type="text/javascript"
-        ></script>
-        <script src="./static/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-        <script
-          src="https://kit.fontawesome.com/f000b2613b.js"
-          crossorigin="anonymous"
-        ></script>
-        <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-        <script src="./static/assets/js/plugins/bootstrap-switch.js"></script>
-        <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-        <script
-          src="./static/assets/js/plugins/nouislider.min.js"
-          type="text/javascript"
-        ></script>
-        <!-- Chart JS -->
-        <script src="./static/assets/js/plugins/chartjs.min.js"></script>
-        <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
-        <script src="./static/assets/js/plugins/moment.min.js"></script>
-        <script
-          src="./static/assets/js/plugins/bootstrap-datetimepicker.js"
-          type="text/javascript"
-        ></script>
-        <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
-        <script
-          src="./static/assets/js/blk-design-system.min.js?v=1.0.0"
-          type="text/javascript"
-        ></script>
+          </div>
+        </footer>
+      </div>
+            <!--   Core JS Files   -->
+            <script
+              src="./static/assets/js/core/jquery.min.js"
+              type="text/javascript"
+            ></script>
+            <script
+              src="./static/assets/js/core/popper.min.js"
+              type="text/javascript"
+            ></script>
+            <script
+              src="./static/assets/js/core/bootstrap.min.js"
+              type="text/javascript"
+            ></script>
+            <script src="./static/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+            <script
+              src="https://kit.fontawesome.com/f000b2613b.js"
+              crossorigin="anonymous"
+            ></script>
+            <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+            <script src="./static/assets/js/plugins/bootstrap-switch.js"></script>
+            <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+            <script
+              src="./static/assets/js/plugins/nouislider.min.js"
+              type="text/javascript"
+            ></script>
+            <!-- Chart JS -->
+            <script src="./static/assets/js/plugins/chartjs.min.js"></script>
+            <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+            <script src="./static/assets/js/plugins/moment.min.js"></script>
+            <script
+              src="./static/assets/js/plugins/bootstrap-datetimepicker.js"
+              type="text/javascript"
+            ></script>
+            <!-- Control Center for Black UI Kit: parallax effects, scripts for the example pages etc -->
+            <script
+              src="./static/assets/js/blk-design-system.min.js?v=1.0.0"
+              type="text/javascript"
+            ></script>
     """
     return {"html_footer": html_data}
 
